496cafaa95151dea622cab15209f7880f49fc30f
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2024-09-17 03:08:37 -0700

    updated action name

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,8 @@ import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder, { action as createOrderAction } from './features/order/CreateOrder';
-import { action as updateOrderAction } from './features/order/UpdateOrder';
+import { action as addOrderPriority } from './features/order/UpdateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
-
 
 const router = createBrowserRouter([
 	{
@@ -42,7 +41,7 @@ const router = createBrowserRouter([
 				element: <Order />,
 				loader: orderLoader,
 				errorElement: <Error />,
-				action: updateOrderAction
+				action: addOrderPriority
 			}
 		]
 	}
